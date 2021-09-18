a1db81d8f8b400a8d05eca17df9e31de45693324
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-09-18 16:23:56 +0200

    Add intial failling tests

--- Assignment02.Tests/StudentTest.cs
+++ Assignment02.Tests/StudentTest.cs
@@ -1,14 +1,36 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Assignment02.Tests
 {
     public class StudentTest
     {
-        [Fact]
-        public void ToStringTest()
+        public static TheoryData<Student, Status> CtorStudents => new TheoryData<Student, Status>
         {
-            var student = new Student(1, "Peter","Jensen");
+            {new Student {Id = 1, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(1)}, Status.New},
+            {new Student {Id = 2, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now}, Status.Active},
+            {new Student {Id = 2, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now, EndDate = DateTime.Now}, Status.Dropout},
+            {new Student {Id = 2, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now, GraduationDate = DateTime.Now}, Status.Graduated}
+        };
+
+        [Theory]
+        [MemberData(nameof(CtorStudents))]
+        public void Ctor_StatusWhenProvidingDateTimes_StatusIsCorrectPropertiesIsSet(Student student, Status expectedStatus)
+        {
+            Assert.True(0 < student.Id);
+            Assert.NotEmpty(student.GivenName);
+            Assert.NotNull(student.GivenName);
+            Assert.NotEmpty(student.Surname);
+            Assert.NotNull(student.Surname);
+            Assert.NotEqual(DateTime.MinValue, student.StartDate);
+            Assert.Equal(expectedStatus, student.Status);
+        }
+
+        [Fact]
+        public void ToString_NewOrActiveStudent_ReturnStringWithGeneralInfoAndStartDate()
+        {
+            var student = new Student{Id = 1, GivenName = "Peter", Surname = "Jensen", StartDate = DateTime.Now};
             string expected = $"Id: 1 Name: Peter Jensen Status: New Start date: {student.StartDate}";
             Assert.Equal(expected,student.ToString());
         }
