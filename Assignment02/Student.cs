1242a9df5efd7ba336ed7f40d5b5a9ac9920c4d6
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-09-18 16:16:39 +0200

    Remove ctor

--- Assignment02/Student.cs
+++ Assignment02/Student.cs
@@ -5,25 +5,6 @@ namespace Assignment02
 {
     public class Student
     {
-        public Student(int id, string givenName, string surname)
-        {
-            Id = id;
-            GivenName = givenName;
-            Surname = surname;
-            StartDate = DateTime.Now;
-        }
-
-        public Student(int id, string givenName, string surname,
-                        DateTime startDate, DateTime endDate, DateTime graduationDate)
-        {
-            Id = id;
-            GivenName = givenName;
-            Surname = surname;
-            StartDate = startDate;
-            EndDate = endDate;
-            GraduationDate = graduationDate;
-        }
-
         public int Id { get; init; }
         public string GivenName { get; set; }
         public string Surname { get; set; }
