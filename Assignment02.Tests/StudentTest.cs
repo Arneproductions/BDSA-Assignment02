8daa97430757ea8b14260928c51e3cd98433f99b
Arne4545 <andreotietgen@hotmail.com>
2021-09-20 17:53:20 +0200

    Implement Student (#5)
    * Implemented student class partially
    
    * Remove not finished implementation from student
    
    * Remove ctor
    
    * Add intial failling tests
    
    * Change input of ctor test
    
    * Implement Student Status getter logic
    
    * Add test for Student Bizz logic
    
    * Rollback to simple test class
    
    * Add extensive test
    
    * Implement Student states
    
    * Fix string test
    
    * Fix test by using today instead of now
    
    Co-authored-by: Daniel <dlli@itu.dk>

---
+++ Assignment02.Tests/StudentTest.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Assignment02.Tests
+{
+    public class StudentTest
+    {
+        public static TheoryData<Student, Status> CtorStudents => new TheoryData<Student, Status>
+        {
+            {new Student {Id = 1, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(1)}, Status.New},
+            {new Student {Id = 2, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1)}, Status.Active},
+            {new Student {Id = 3, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now}, Status.Dropout},
+            {new Student {Id = 4, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), GraduationDate = DateTime.Now}, Status.Graduated},
+            {new Student {Id = 5, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now.AddDays(-2), GraduationDate = DateTime.Now}, Status.Graduated},
+            {new Student {Id = 6, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now.AddDays(3), GraduationDate = DateTime.Now}, Status.Graduated},
+            {new Student {Id = 7, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now.AddDays(-1), GraduationDate = DateTime.Now}, Status.Dropout},
+            {new Student {Id = 7, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Today, EndDate = DateTime.Today, GraduationDate = DateTime.Today}, Status.Graduated}
+        };
+
+        [Theory]
+        [MemberData(nameof(CtorStudents))]
+        public void Ctor_StatusWhenProvidingDateTimes_StatusIsCorrectPropertiesIsSet(Student student, Status expectedStatus)
+        {
+            Assert.True(0 < student.Id);
+            Assert.NotEmpty(student.GivenName);
+            Assert.NotNull(student.GivenName);
+            Assert.NotEmpty(student.Surname);
+            Assert.NotNull(student.Surname);
+            Assert.NotEqual(DateTime.MinValue, student.StartDate);
+            Assert.Equal(expectedStatus, student.Status);
+        }
+
+
+
+        [Fact]
+        public void ToString_NewOrActiveStudent_ReturnStringWithGeneralInfoAndStartDate()
+        {
+            var student = new Student{Id = 1, GivenName = "Peter", Surname = "Jensen", StartDate = DateTime.Now};
+            string expected = $"Id: 1, Name: Peter Jensen, Status: Active, Start date: {student.StartDate}, End date {student.EndDate}, Graduation date {student.GraduationDate}";
+            Assert.Equal(expected,student.ToString());
+        }
+        
+        
+    }
+}
---
+++ Assignment02/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+
+namespace Assignment02
+{
+    public class Student
+    {
+        public int Id { get; init; }
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public Status Status 
+        { 
+            get
+            {
+                Status status;
+
+                DateTime now = DateTime.Now;
+                if(StartDate > now)
+                    status = Status.New;
+                else
+                    status = Status.Active;
+
+                if(Graduates && GraduationDate <= now)
+                    status = Status.Graduated;
+                else if(EndDate >= StartDate && EndDate <= now)
+                    status = Status.Dropout;
+
+                return status;
+            } 
+        }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public DateTime GraduationDate { get; set; }
+        private DateTime _graduationDate;
+
+        private bool Graduates => GraduationDate >= StartDate && ((EndDate < StartDate && GraduationDate > EndDate) || (EndDate >= StartDate && GraduationDate <= EndDate));
+        
+
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {GivenName} {Surname}, Status: {Status}, Start date: {StartDate}, End date {EndDate}, Graduation date {GraduationDate}";
+        }
+    }
+}
