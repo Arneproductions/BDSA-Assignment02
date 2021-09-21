b7fb18c6acd12814f10337d3a7db5a4869e721e1
Arne4545 <andreotietgen@hotmail.com>
2021-09-21 14:12:33 +0200

    Feature/immutable student (#6)
    * Implemented student class partially
    
    * Remove not finished implementation from student
    
    * Remove ctor
    
    * Add intial failling tests
    
    * Change input of ctor test
    
    * Implement Student Status getter logic
    
    * Add test for Student Bizz logic
    
    * Rollback to simple test class
    
    * Add extensive test
    
    * Implement Student states
    
    * Fix string test
    
    * Fix test by using today instead of now
    
    * Add test to the immutable student
    
    * Add immutablestudent test
    
    * Change name of variable
    
    * Implement test on equal
    
    Co-authored-by: Daniel <dlli@itu.dk>

---
+++ Assignment02.Tests/ImmutableStudentTest.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Assignment02.Tests
+{
+    public class ImmutableStudentTest
+    {
+        public static TheoryData<ImmutableStudent, Status> CtorImmutableStudents => new TheoryData<ImmutableStudent, Status>
+        {
+            {new ImmutableStudent {Id = 1, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(1)}, Status.New},
+            {new ImmutableStudent {Id = 2, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1)}, Status.Active},
+            {new ImmutableStudent {Id = 3, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now}, Status.Dropout},
+            {new ImmutableStudent {Id = 4, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), GraduationDate = DateTime.Now}, Status.Graduated},
+            {new ImmutableStudent {Id = 5, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now.AddDays(-2), GraduationDate = DateTime.Now}, Status.Graduated},
+            {new ImmutableStudent {Id = 6, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now.AddDays(3), GraduationDate = DateTime.Now}, Status.Graduated},
+            {new ImmutableStudent {Id = 7, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Now.AddDays(-1), EndDate = DateTime.Now.AddDays(-1), GraduationDate = DateTime.Now}, Status.Dropout},
+            {new ImmutableStudent {Id = 7, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Today, EndDate = DateTime.Today, GraduationDate = DateTime.Today}, Status.Graduated}
+        };
+
+        [Theory]
+        [MemberData(nameof(CtorImmutableStudents))]
+        public void Ctor_StatusWhenProvidingDateTimes_StatusIsCorrectPropertiesIsSet(ImmutableStudent ImmutableStudent, Status expectedStatus)
+        {
+            Assert.True(0 < ImmutableStudent.Id);
+            Assert.NotEmpty(ImmutableStudent.GivenName);
+            Assert.NotNull(ImmutableStudent.GivenName);
+            Assert.NotEmpty(ImmutableStudent.Surname);
+            Assert.NotNull(ImmutableStudent.Surname);
+            Assert.NotEqual(DateTime.MinValue, ImmutableStudent.StartDate);
+            Assert.Equal(expectedStatus, ImmutableStudent.Status);
+        }
+
+        [Fact]
+        public void Equal_TwoDifferentInstancesWithSameValues_ReturnTrue()
+        {
+            ImmutableStudent student1 = new ImmutableStudent {Id = 7, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Today, EndDate = DateTime.Today, GraduationDate = DateTime.Today};
+            ImmutableStudent student2 = new ImmutableStudent {Id = 7, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Today, EndDate = DateTime.Today, GraduationDate = DateTime.Today};
+            
+            Assert.Equal(student1, student2);
+        }
+        
+        [Fact]
+        public void Equal_InstanceEqualsItself_ReturnTrue()
+        {
+            ImmutableStudent student1 = new ImmutableStudent {Id = 7, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Today, EndDate = DateTime.Today, GraduationDate = DateTime.Today};
+            
+            Assert.Equal(student1, student1);
+        }
+
+        [Fact]
+        public void Equal_TwoDifferentInstancesWithDifferentValues_ReturnFalse()
+        {
+            ImmutableStudent student1 = new ImmutableStudent {Id = 8, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Today, EndDate = DateTime.Today, GraduationDate = DateTime.Today};
+            ImmutableStudent student2 = new ImmutableStudent {Id = 7, GivenName = "Test", Surname = "Testesen", StartDate = DateTime.Today, EndDate = DateTime.Today, GraduationDate = DateTime.Today};
+            
+            Assert.NotEqual(student1, student2);
+        }
+
+
+        [Fact]
+        public void ToString_NewOrActiveImmutableStudent_ReturnStringWithGeneralInfoAndStartDate()
+        {
+            var immutableStudent = new ImmutableStudent{Id = 1, GivenName = "Peter", Surname = "Jensen", StartDate = DateTime.Now};
+            string expected = $"Id: 1, Name: Peter Jensen, Status: Active, Start date: {immutableStudent.StartDate}, End date {immutableStudent.EndDate}, Graduation date {immutableStudent.GraduationDate}";
+            Assert.Equal(expected,immutableStudent.ToString());
+        }
+        
+        
+    }
+}
